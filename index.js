b1bf1f6ee5bdaf2f25c5783af5abe0eeb391caef
AkibKST <akibkst22@gmail.com>
2024-03-04 18:07:14 +0600

    use query parameters to load user specefic data

--- index.js
+++ index.js
@@ -45,11 +45,26 @@ async function run() {
     app.get('/services/:id', async(req, res)=>{
         const id = req.params.id;
         const query = {_id: new ObjectId(id)};
+
+        const options = {
+          projection: { title: 1, price: 1, service_id: 1, img: 1, customerName: 1, email: 1, service: 1}
+        };
+
         const result = await serviceCollection.findOne(query);
         res.send(result);
     })
 
     // bookings
+
+    app.get('/bookings', async(req, res) =>{
+      let query = {};
+      if(req.query.email){
+        query = { email: req.query.email }
+      }
+      const result = await bookingCollection.find(query).toArray();
+      res.send(result)
+    })
+
     app.post('/bookings', async(req, res)=>{
         const booking = req.body;
         console.log(booking);
